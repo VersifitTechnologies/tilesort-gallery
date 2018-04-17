113a4267b17b565ef43bb547fe12730384858b20
VERSIFIT\vangk <kong.vang@hoonuit.com>
2018-04-17 10:29:50 -0500

    don't send back a clone of scope

--- dist/tilesort-gallery.js
+++ dist/tilesort-gallery.js
@@ -145,7 +145,7 @@ angular.module('tilesortGallery', ['ui.bootstrap']).controller('TilesortModalCtr
                     windowClass: 'tilesort-modal modal-fit',
                     resolve: {
                         scope: function () {
-                            return _.cloneDeep(scope);
+                            return scope;
                         },
                         image: function() {
                             return image;
